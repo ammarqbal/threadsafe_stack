12ae27d6aa25fc9c4c2b1f92c5ba4a0d027bdd18
Ammar Iqbal <AmmarIqbal@protonmail.com>
2026-08-02 00:15:36 +0500

    feat: implemented wait_and_pop
    - used `std::condition_variable`

--- include/threadsafe_stack.hpp
+++ include/threadsafe_stack.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <condition_variable>
 #include <mutex>
 #include <optional>
 #include <stack>
@@ -12,20 +13,27 @@ public:
   threadsafe_stack &operator=(const threadsafe_stack &copy) = delete;
 
   threadsafe_stack(threadsafe_stack &&other) = delete;
-  threadsafe_stack &operator=(threadsafe_stack &other) = delete;
+  threadsafe_stack &operator=(threadsafe_stack &&other) = delete;
 
   void push(T value);
   bool try_pop(T &value);
   [[nodiscard]] std::optional<T> try_pop();
+  void wait_and_pop(T &value);
+  bool empty() const;
 
 private:
   std::stack<T> data_;
   mutable std::mutex m_;
+  std::condition_variable cv_;
 };
 
 template <typename T> void threadsafe_stack<T>::push(T value) {
-  std::lock_guard<std::mutex> lk(m_);
-  data_.push(std::move(value));
+  {
+    std::lock_guard<std::mutex> lk(m_);
+    data_.push(std::move(value));
+  }
+
+  cv_.notify_one();
 }
 
 template <typename T> bool threadsafe_stack<T>::try_pop(T &value) {
@@ -46,4 +54,16 @@ template <typename T> std::optional<T> threadsafe_stack<T>::try_pop() {
   T value = std::move(data_.top());
   data_.pop();
   return value;
+}
+
+template <typename T> void threadsafe_stack<T>::wait_and_pop(T &value) {
+  std::unique_lock<std::mutex> lk(m_);
+  cv_.wait(lk, [this]() { return !data_.empty(); });
+  value = std::move(data_.top());
+  data_.pop();
+}
+
+template <typename T> bool threadsafe_stack<T>::empty() const {
+  std::lock_guard<std::mutex> lk(m_);
+  return data_.empty();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <threadsafe_stack.hpp>
+#include <vector>
 int main() {
   threadsafe_stack<int> my_stack;
-  std::thread t1([&]() {
-    for (int i{0}; i < 100000; i++) {
+  std::thread producer([&my_stack]() {
+    for (int i{0}; i < 8; i++) {
+      my_stack.push(-1);
+    }
+
+    for (int i{0}; i < 1000000; i++) {
       my_stack.push(i);
     }
   });
-  int count{0};
-  int value{};
-  t1.join();
+  producer.join();
 
-  std::thread t2([&]() {
-    while (my_stack.try_pop(value)) {
-      count++;
-    }
-  });
-  t2.join();
-  std::cout << count << std::endl;
+  std::mutex ctm;
+  int count{0};
+  std::vector<std::thread> workers;
+  for (int i{0}; i < 8; i++) {
+    workers.push_back(std::thread([&count, &ctm, &my_stack]() {
+      int value{0};
+      while (true) {
+        my_stack.wait_and_pop(value);
+        if (value == -1) {
+          return;
+        }
+        {
+          std::lock_guard<std::mutex> lk(ctm);
+          count++;
+        }
+      }
+    }));
+  }
+
+  for (auto &worker : workers) {
+    worker.join();
+  }
+  std::cout << count << '\n';
 }
